4d1a725f21f0bed8972f838bac6d06c86f95fc84
Ansis Kalnyn <ansis.kalnyn@yandex.ru>
2023-10-09 07:26:27 +0300

    Added limits (single/month).

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,23 +4,58 @@ import java.text.DecimalFormat
 
 fun main() {
 
-    val amount = 15000
-    var sumAllTransfers = 75
-    val type = "Mir"
-    val numberViewTax = DecimalFormat("#.##").format(calcTaxTransfer(type, sumAllTransfers, amount));
-    sumAllTransfers += amount
-    println(
-        """Комиссия за перевод: $amount р. с карты $type составит: $numberViewTax р.
-        |Сумма всех Ваших переводов (на текущий момент): $sumAllTransfers р.
-    """.trimMargin()
-    )
+    val amount = 50_000
+    var sumAllTransfers = 590_000
+    val type = "MasterCard"
+    val limitLevel = checkLimits(type, sumAllTransfers, amount)
+
+    if (limitLevel == 0) {
+
+        val numberViewTax = DecimalFormat("#.##").format(calcTaxTransfer(type, sumAllTransfers, amount));
+        sumAllTransfers += amount
+        println(
+            """Комиссия за перевод: $amount р. с карты $type составит: $numberViewTax р.
+                |Сумма всех Ваших переводов (на текущий месяц): $sumAllTransfers р.
+            """.trimMargin()
+        )
+
+    } else {
+
+        when (limitLevel) {
+
+            1 -> println("Превышена максимально допустимая сумма перевода.")
+            2 -> println("Превышен месячный лимит переводов.")
+            3 -> println("Сумма перевода больше доступного остатка по лимиту переводов в месяц.")
+        }
+
+    }
+
+}
+
+
+fun checkLimits(type: String = "VK Pay", sumAllTransfers: Int, amount: Int): Int = when (type) {
+
+    "VK Pay" -> when {
+        amount > 15_000 -> 1
+        sumAllTransfers > 40_000 -> 2
+        sumAllTransfers + amount > 40_000 -> 3
+        else -> 0
+    }
+
+    else -> when {
+        amount > 150_000 -> 1
+        sumAllTransfers > 600_000 -> 2
+        sumAllTransfers + amount > 600_000 -> 3
+        else -> 0
+    }
+
 }
 
 fun calcTaxTransfer(type: String = "VK Pay", sumAllTransfers: Int = 0, amount: Int): Double {
 
     val masterCardMaestroTaxRate = 0.006
     val masterCardMaestroTaxPlus = 20
-    val masterCardMaestroMaxUnTaxLimit = 75000
+    val masterCardMaestroMaxUnTaxLimit = 75_000
     val visaMirTaxRate = 0.0075
     val visaMirTaxMin = 35
 
